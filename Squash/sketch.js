4edbaefdc465f6d76c5e4fdaeb9e346aea3d396d
unknown <raveen.anand@whitehatjr.com>
2021-11-03 13:38:16 +0530

    v2

--- Squash/sketch.js
+++ Squash/sketch.js
@@ -1,19 +1,15 @@
-var racket, ball;
+var racket, ball, points;
 var wallTop, wallBottom, wallLeft, wallRight;
 
-var losingSound;
-
-function preload() {
-  losingSound = loadSound("Assets/losing.wav");
-}
 
 function setup() {
   createCanvas(450, 300);
 
+  points=0; 
+
   //paddle sprite
   racket = createSprite(200, 250, 60, 7);
   racket.shapeColor = "darkgray";
-  racket.immovable = true;
 
   //ball sprite
   ball = createSprite(200, 150, 7, 7);
@@ -22,45 +18,45 @@ function setup() {
   //Top edge
   wallTop = createSprite(225, 0, 450, 20);
   wallTop.shapeColor = "navajowhite";
-  wallTop.immovable = true;
 
   //Bottom edge
   wallBottom = createSprite(225, 300, 450, 5);
   wallBottom.shapeColor = "navajowhite";
-  wallBottom.immovable = true;
 
   //Left edge
   wallLeft = createSprite(0, 200, 5, 400);
   wallLeft.shapeColor = "navajowhite";
-  wallLeft.immovable = true;
 
   //Right edge
   wallRight = createSprite(450, 200, 5, 400);
   wallRight.shapeColor = "navajowhite";
-  wallRight.immovable = true;
 }
 
 function draw() {
   background("beige");
 
-  racket.position.x = mouseX;
+  text("Score : ", 360, 30);
+  text(points, 400, 30);
+
+  racket.x = mouseX;
 
   if (mouseIsPressed) {
-    ball.velocity.x = 1; // Left or Right
-    ball.velocity.y = 1; // Top to Bottom
-    ball.setSpeed(5);
+    ball.velocityX = 5; 
+    ball.velocityY = 5; 
   }
 
-  ball.bounce(wallLeft);
-  ball.bounce(wallRight);
-  ball.bounce(racket);
+  ball.bounceOff(wallLeft);
+  ball.bounceOff(wallRight);
+  ball.bounceOff(racket);
 
-  ball.bounce(wallBottom, endgame);
+  
+  ball.bounceOff(wallBottom, endgame);
 
   drawSprites();
 }
 
-function endgame(ball) {
-  ball.remove();
-  losingSound.play();
+function endgame() {
+  ball.velocityX=0;
+  ball.velocityY=0;
 }
+
